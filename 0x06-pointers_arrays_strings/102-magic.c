3f5293649cc03c7d77b4540807f7ce98a9b7e932
root <sophiannadi721@gmail.com>
2023-05-04 13:15:19 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,24 +1,23 @@
-#include "main.h"
 #include <stdio.h>
-int _putchar(char);
+
+int main(void)
 {
 int n;
-int a[2];
+int a[5];
 int *p;
 
 a[2] = 1024;
 p = &n;
-/*
-* line of code here...
-* Remember:
-* - am not allowed to use a
-* - am not allowed to modify p
-* - am not allowed to code anything else than this line of code.
-*/
-{
-*(p + 2) = 98;
-*std::cout << "a[2] = " << *(p + 2) << std::endl;
-/* ...so that this prints 98\n */
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+*(p + 5) = 98;
+  /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
